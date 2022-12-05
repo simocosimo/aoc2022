0ea231953507fa4a69eb140ad05b9f0dc68e1f60
Simone Cosimo <simosx4@gmail.com>
2022-12-05 19:03:23 +0100

    Solved day 5!

---
+++ aoc05_supply_stacks/aoc05_supply_stacks.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Simone Cosimo"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["aoc05_supply_stacks"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.10"
---
+++ aoc05_supply_stacks/src/aoc05_supply_stacks.nim
@@ -0,0 +1,36 @@
+import streams
+import strutils
+
+const cols = 9 
+var strm = newFileStream("input.txt", fmRead)
+var line = ""
+var 
+  inputMode = 0
+  stacks: seq[seq[char]] = newSeq[seq[char]](cols)
+
+if not isNil(strm):
+  while strm.readLine(line):
+    if line.isEmptyOrWhitespace(): continue
+    if inputMode == 0:  # reading stacks
+      if line[0..1] == " 1":
+        inputMode = 1
+        continue
+
+      var i: int = 0
+      for c in countup(1, line.len() - 1, 4):
+        if line[c] != ' ': stacks[i].add(line[c])
+        i += 1
+        i = i mod cols
+    else:   # executing moves
+      var 
+        count = parseInt(line[5..<line.len()].split(" from ")[0])
+        f = parseInt(line[5..<line.len()].split(" from ")[1].split(" to ")[0]) - 1
+        to = parseInt(line[5..<line.len()].split(" from ")[1].split(" to ")[1]) - 1
+
+      for i in 0..<count:
+        stacks[to].insert(stacks[f][0], 0)
+        stacks[f].delete(0)
+
+for i in 0..<cols:
+  stdout.write(stacks[i][0])
+stdout.write('\n')
