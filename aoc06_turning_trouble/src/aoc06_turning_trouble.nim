f9884deb74188493756ca4417d894963880aac17
Simone Cosimo <simosx4@gmail.com>
2022-12-06 11:19:55 +0100

    Day 6 is done! bip bop xmas bop bip

---
+++ aoc06_turning_trouble/src/aoc06_turning_trouble.nim
@@ -0,0 +1,30 @@
+import streams
+import deques
+import sequtils
+
+const queueSize = 4
+var
+  strm = newFileStream("input.txt", fmRead)
+  line = ""
+  queue = initDeque[char](0) 
+
+proc checkQueue(q: Deque): bool =
+  let unique = deduplicate(toOpenArray(toSeq(q.items), 0, q.len() - 1))
+  result = unique.len() == q.len()
+
+if not isNil(strm):
+  line = strm.readLine()
+  for i in 0..<queueSize:
+    queue.addLast(line[i])
+
+  if not checkQueue(queue):
+    # now I have the starting queue set up
+    for i in queueSize..<line.len():
+      queue.popFirst()
+      queue.addLast(line[i])
+      if checkQueue(queue):
+        echo i + 1 
+        echo queue
+        break
+  else:
+    echo queueSize 
